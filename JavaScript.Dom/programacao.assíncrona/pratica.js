a1aeca17ee353da8854e5c99b89ed9e292acef58
joaomarcos-luz <joaomarcossoaresluz9@gmail.com>
2024-03-30 14:45:10 -0300

    praticas callback

--- JavaScript.Dom/programacao.assíncrona/pratica.js
+++ JavaScript.Dom/programacao.assíncrona/pratica.js
@@ -44,3 +44,64 @@ const callback = (resultado) => console.log(resultado)
 
 somarAsyn(10, 15, callback)//So ira execultar se o callbeck for passado como parametro.
 
+
+/*Imagine que quero usar somarAsync para fazer 10 + 20 + 30 + 40 + 50. Neste caso, a solução poderia ser implementada dessa forma:*/
+//Exemplo 01
+
+const somarAsync = (a, b, callback) => {
+    setTimeout(() => {
+        const resultado = a + b;
+        callback(resultado);
+    }, 1000);
+};
+
+somarAsync(10, 20, (res1) => {
+    somarAsync(res1, 30, (res2) => {
+        somarAsync(res2, 40, (res3) => {
+            somarAsync(res3, 50, (resultado) => {
+                console.log(resultado);
+            });
+        });
+    });
+});
+
+//Exemplo 02
+const soma = (a, b, callback) => setTimeout(() => {
+    if(callback) callback(a + b)
+} ,1000)
+
+soma(10, 20, (res1) => {
+    soma(res1, 30, (res2) => {
+        soma(res2, 50, (res3) => {
+            soma(res3, 90, (resultado) => {
+                console.log(resultado)
+            })
+        })
+    })
+})
+
+//Exemplo 03
+const somar = (a, b , callback) => setTimeout(() => {
+    const resultado = a + b
+    callback(resultado)
+} ,1000)
+
+somar(10, 10, (res1) => {
+    somar(res1, 20, (res2) => {
+        somar(res2, 50, (res3) => {
+            somar(res3, 80, (resultado) => {
+                console.log(resultado)
+            })
+        })
+    })
+})
+
+
+//Exemplo
+const soma = (a, b, callback) => setTimeout(() => {
+    if(callback) callback(a + b)
+} ,1000)
+
+const callback = (res) => console.log(res)
+
+soma(5, 8, callback)
