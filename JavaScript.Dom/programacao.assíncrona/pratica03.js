e348149ea2d2fac30f3f8ea54b26eb29f0ef04a7
joaomarcos-luz <joaomarcossoaresluz9@gmail.com>
2024-04-01 11:49:43 -0300

    exemplos async

--- JavaScript.Dom/programacao.assíncrona/pratica03.js
+++ JavaScript.Dom/programacao.assíncrona/pratica03.js
@@ -1,2 +1,65 @@
 //Utilizando funções assíncronas por: async/await.
 
+//Exemplo 01
+const somarAsync = async (a, b) => {
+    return new Promise((resposta, erro) => {
+        setTimeout(() => {
+            const res = a + b
+            resposta(res)
+        } ,1000)
+    })
+}
+
+const calcular = async function () {
+    const res = await somarAsync(10, 20)
+    console.log(res)
+}
+
+calcular()
+
+
+//Exemplo 02
+const somarAsync = async (a, b) => {
+    return new Promise((resposta, reject) => {
+        setTimeout(() => {
+            const res = a + b
+            resposta(res)
+        },1000)
+    })
+}
+
+const calcular = async () => {
+    const res1 = await somarAsync(10, 20)
+    const res2 = await somarAsync(res1, 30)
+    const res3 = await somarAsync(res2, 40)
+    const resultado = await somarAsync(res3, 50)
+    console.log(resultado)
+}
+
+calcular()
+
+
+//Exemplo 03
+const somarAsync = async (a, b) => {
+    return new Promise((resposta, reject) => {
+        setTimeout(() => {
+            const res = a + b
+            resposta(res)
+        },1000)
+    })
+}
+
+const calcular = async () => {
+    const res1 = await somarAsync(10, 20)
+    const res2 = await somarAsync(res1, 30)
+    const res3 = await somarAsync(res2, 40)
+    const resultado = await somarAsync(res3, 50)
+    console.log(resultado)
+}
+
+(async () => {
+    await calcular();
+})();
+
+
+//Exemplo 04
